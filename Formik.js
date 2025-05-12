10f6e55fe0f816347958bbd3b203630db9bff756
carmenquesada <116158072+carmenquesada@users.noreply.github.com>
2025-05-12 15:55:56 +0200

    Update Formik.js

--- Formik.js
+++ Formik.js
@@ -1,22 +1,24 @@
 import { Formik } from 'formik'
 
+// Este componente es una pantalla para crear un nuevo restaurante. 
+// Muestra un formulario con campos como nombre, descripción, dirección, etc. Y utiliza Formik.
 
-export default function CreateRestaurantScreen () {
+export default function CreateRestaurantScreen () { // Definir los valores iniciales del formulario
   const initialRestaurantValues = { name: null, description: null, address: null, postalCode: null, url: null, shippingCosts: null, email: null, phone: null, restaurantCategoryId: null }
 
   // Rest of the code of this component
   // ...
 
-  return (
+  return ( // Formik como componente principal del formulario. Se le pasan los valores iniciales con initialValues
     <Formik
     initialValues={initialRestaurantValues}
     >
-      {({ setFieldValue, values }) => (
+      {({ setFieldValue, values }) => ( //setFielValue cambia el valor de un campo específico y values contiene todos los valores actuales del formulario
         <ScrollView>
           <View style={{ alignItems: 'center' }}>
             <View style={{ width: '60%' }}>
-              <InputItem
-                name='name'
+              <InputItem //InputItem es un componente personalizado para mostrar un campo de entrada (como un TextInput) con su etiqueta. 
+                name='name' // El atributo name='name' le dice a Formik que ese campo representa el nombre del restaurante.
                 label='Name:'
               />
 
